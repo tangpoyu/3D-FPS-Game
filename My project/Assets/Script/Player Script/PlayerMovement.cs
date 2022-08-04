a948676f4319b699da99dddee4da616b68abf602
boyu <b10623041@gemail.yuntech.edu.tw>
2022-08-04 23:32:56 +0800

    closes #4; FPS Mouse Look And Mouse Controls

--- My project/Assets/Script/Player Script/PlayerMovement.cs
+++ My project/Assets/Script/Player Script/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController characterController;
     private Vector3 move_Direction;
     public float speed = 5f;
-    private float gravity = 20f;
+    private float gravity = 5f;
     public float jump_Forse = 10f;
-    private float vertical_Velocity;
+    private float y_Energy;
 
     private void Awake()
     {
@@ -26,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     private void MoveThePlayer()
     {
         move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+        // convert move_direction from worldSpace to localSpace
         move_Direction = transform.TransformDirection(move_Direction);
         move_Direction *= speed * Time.deltaTime;
         ApplyGravity();
@@ -34,15 +35,15 @@ public class PlayerMovement : MonoBehaviour
 
     private void ApplyGravity()
     {
-        vertical_Velocity -= gravity * Time.deltaTime;
+        y_Energy -= gravity * Time.deltaTime;
         PlayerJump();
-        move_Direction.y = vertical_Velocity * Time.deltaTime;
+        move_Direction.y = y_Energy;
     }
 
     private void PlayerJump()
     {
        if(characterController.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
-            vertical_Velocity = jump_Forse;
+            y_Energy = jump_Forse * Time.deltaTime;
         }
     }
 }
