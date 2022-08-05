c2548306e462a66cfd6b9681aac74452919840bd
boyu <b10623041@gemail.yuntech.edu.tw>
2022-08-05 14:11:14 +0800

    closes #5; Player's Sprint And Crouch

--- My project/Assets/Script/Player Script/PlayerMovement.cs
+++ My project/Assets/Script/Player Script/PlayerMovement.cs
@@ -7,14 +7,25 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
     private Vector3 move_Direction;
-    public float speed = 5f;
+    public float speed;
+    public float moveSpeed;
+    public float sprint_Speed = 10f;
+    public float crouch_Speed = 2f;
+  
     private float gravity = 5f;
-    public float jump_Forse = 10f;
+    public float jump_Forse = 1.33f;
     private float y_Energy;
+
+    private Transform look_Root;
+    private float stand_Height = 0.98f;
+    private float crouch_Height = 0.44f;
+    private bool is_Crouching = false;
+
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        look_Root = transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -28,9 +39,28 @@ public class PlayerMovement : MonoBehaviour
         move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
         // convert move_direction from worldSpace to localSpace
         move_Direction = transform.TransformDirection(move_Direction);
+        Crouch();
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching) speed = sprint_Speed;
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching) speed = moveSpeed;
         move_Direction *= speed * Time.deltaTime;
         ApplyGravity();
         characterController.Move(move_Direction);
+    }
+
+    private void Crouch()
+    {
+       if (Input.GetKeyDown(KeyCode.LeftControl) )
+        {
+            is_Crouching = true;
+            look_Root.localPosition = new Vector3(0, crouch_Height, 0);
+            speed = crouch_Speed;
+        }
+
+       if (Input.GetKeyUp(KeyCode.LeftControl)) {
+            is_Crouching = false;
+            look_Root.localPosition = new Vector3(0, stand_Height, 0);
+            speed = moveSpeed;
+        }
     }
 
     private void ApplyGravity()
@@ -43,7 +73,7 @@ public class PlayerMovement : MonoBehaviour
     private void PlayerJump()
     {
        if(characterController.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
-            y_Energy = jump_Forse * Time.deltaTime;
+                 y_Energy = jump_Forse;
         }
     }
 }
