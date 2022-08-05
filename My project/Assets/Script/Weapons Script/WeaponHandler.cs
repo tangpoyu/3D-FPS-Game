3a93aaf45b45ae4688c9b9ba56c0a95e09ef7f6d
boyu <b10623041@gemail.yuntech.edu.tw>
2022-08-05 23:14:54 +0800

    closes #8; Weapon Handler (script for controlling the attributes of weapons)

---
+++ My project/Assets/Script/Weapons Script/WeaponHandler.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim
+{
+    NONE,
+    SELF_AIM,
+    AIM
+}
+
+public enum WeaponFireType
+{
+    SINGLE,
+    MULTIPLE
+}
+
+public enum WeaponBulletType
+{
+    BULLET,
+    ARROW,
+    SPEAR,
+    NONE
+}
+
+public class WeaponHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private WeaponAim weaponAim;
+    [SerializeField]
+    private WeaponFireType weaponFireType;
+    [SerializeField]
+    private WeaponBulletType weaponBulletType;
+
+    [SerializeField]
+    private GameObject muzzleFlash;
+
+    [SerializeField]
+    private AudioSource shootSound, reloadSound;
+
+    public GameObject attackPoint;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Shoot()
+    {
+       animator.SetTrigger(AnimationTags.SHOOT_TRIGGER);
+    }
+
+    public void Aim(bool isAim)
+    {
+        animator.SetBool(AnimationTags.AIM_PARAMETER, isAim);
+    }
+
+    public void TurnOnMuzzleFlash()
+    {
+        muzzleFlash.SetActive(true);
+    }
+
+    public void TurnOffMuzzleFlash()
+    {
+        muzzleFlash.SetActive(false);
+    }
+
+    public void PlayShootSound()
+    {
+        shootSound.Play();
+    }
+
+    public void PlayReloadSound()
+    {
+        reloadSound.Play();
+    }
+
+    public void TurnOnAttackPoint()
+    {
+        attackPoint.SetActive(true);
+    }
+
+    public void TurnOffAttackPoint()
+    {
+        if (attackPoint.activeInHierarchy) attackPoint.SetActive(false);
+    }
+
+}
